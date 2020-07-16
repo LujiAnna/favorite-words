26b5eba91b4b534468eb3f592d0dab7bd742d853
abdulhamid <abdulhamid.ali.996@gmail.com>
2020-07-16 19:39:09 +0200

    closed #5

--- scripts/user-actions/display.js
+++ scripts/user-actions/display.js
@@ -3,7 +3,7 @@ console.log('--- loading: display-all.js');
 
 // reverse engineering!
 //   the handler has no tests, study the demo to write this function
-const displayHandler = (message = '', arrayToDisplay = words) => {
+ const displayHandler = (message = '', arrayToDisplay = words) => {
   debugger;
   const combineStrings = (acc, next) => {
     return acc+'\n'+next;
